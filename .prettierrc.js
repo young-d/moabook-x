30663b58e3a824ff09eeee3cb6c8a339f1c2dc25
young-d <dodoga096@gmail.com>
2022-02-04 00:12:14 +0900

    init: Next app 생성 및 eslint, prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80, 
+  tabWidth: 2, 
+  singleQuote: true, 
+  trailingComma: 'all', 
+  semi: true, 
+  useTabs: false, 
+  endOfLine: 'auto',
+  bracketSpacing: true, 
+  jsxBracketSameLine: true, 
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  const test = 123;
+
+  return <div>{test}</div>;
+};
+
+export default Home;
